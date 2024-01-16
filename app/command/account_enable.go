91d97b246e3f9f947c6a905f699288e715b3cc49
9ssi7 <info@ssibrahimbas.com>
2024-01-16 21:10:50 +0300

    feature/cillop: add enable and disable cmd

--- app/command/account_enable.go
+++ app/command/account_enable.go
@@ -1,1 +1,35 @@
 package command
+
+import (
+	"context"
+
+	"github.com/cilloparch/cillop/cqrs"
+	"github.com/cilloparch/cillop/i18np"
+	"github.com/turistikrota/service.account/domains/account"
+)
+
+type AccountEnableCmd struct {
+	UserUUID string `params:"-"`
+	UserName string `params:"userName" validate:"required,username"`
+}
+
+type AccountEnableRes struct{}
+
+type AccountEnableHandler cqrs.HandlerFunc[AccountEnableCmd, *AccountEnableRes]
+
+func NewAccountEnableHandler(repo account.Repo, events account.Events) AccountEnableHandler {
+	return func(ctx context.Context, cmd AccountEnableCmd) (*AccountEnableRes, *i18np.Error) {
+		err := repo.Enable(ctx, account.UserUnique{
+			UUID: cmd.UserUUID,
+			Name: cmd.UserName,
+		})
+		if err != nil {
+			return nil, err
+		}
+		events.Enabled(account.UserUnique{
+			UUID: cmd.UserUUID,
+			Name: cmd.UserName,
+		})
+		return &AccountEnableRes{}, nil
+	}
+}
