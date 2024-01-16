b65e9e5e23b6462850e2a0f9a03d5e69d1dc11a4
9ssi7 <info@ssibrahimbas.com>
2024-01-16 20:45:55 +0300

    feature/cillop: add the platform domain for first time

--- domains/platform/entity.go
+++ domains/platform/entity.go
@@ -1,1 +1,35 @@
 package platform
+
+import (
+	"time"
+)
+
+type Entity struct {
+	UUID         string                  `json:"uuid" bson:"_id,omitempty"`
+	Name         string                  `json:"name" bson:"name"`
+	Slug         string                  `json:"slug" bson:"slug"`
+	Regexp       string                  `json:"regexp" bson:"regexp"`
+	Prefix       string                  `json:"prefix" bson:"prefix"`
+	Translations map[Locale]Translations `json:"translations" bson:"translations"`
+	IsActive     bool                    `json:"isActive" bson:"is_active"`
+	IsDeleted    bool                    `json:"isDeleted" bson:"is_deleted"`
+	UpdatedAt    time.Time               `json:"updatedAt" bson:"updated_at"`
+	CreatedAt    time.Time               `json:"createdAt" bson:"created_at"`
+}
+
+type Translations struct {
+	Name        string `json:"name"`
+	Placeholder string `json:"placeholder"`
+	Description string `json:"description"`
+}
+
+type Locale string
+
+const (
+	LocaleEN Locale = "en"
+	LocaleTR Locale = "tr"
+)
+
+func (l Locale) String() string {
+	return string(l)
+}
--- domains/platform/fields.go
+++ domains/platform/fields.go
@@ -1,1 +1,47 @@
 package platform
+
+type fieldsType struct {
+	UUID         string
+	Name         string
+	Slug         string
+	Regexp       string
+	Prefix       string
+	Translations string
+	IsActive     string
+	IsDeleted    string
+	UpdatedAt    string
+	CreatedAt    string
+}
+
+type translationFieldType struct {
+	Name        string
+	Placeholder string
+	Description string
+}
+
+var fields = fieldsType{
+	UUID:         "_id",
+	Name:         "name",
+	Slug:         "slug",
+	Regexp:       "regexp",
+	Prefix:       "prefix",
+	Translations: "translations",
+	IsActive:     "is_active",
+	IsDeleted:    "is_deleted",
+	UpdatedAt:    "updated_at",
+	CreatedAt:    "created_at",
+}
+
+var translationFields = translationFieldType{
+	Name:        "name",
+	Placeholder: "placeholder",
+	Description: "description",
+}
+
+func translationFieldInArray(field string) string {
+	return fields.Translations + ".$." + field
+}
+
+func translationField(field string) string {
+	return fields.Translations + "." + field
+}
