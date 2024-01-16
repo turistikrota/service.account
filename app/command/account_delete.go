d6fe2b122ae51ffc31b84485eb33b343743a04b3
9ssi7 <info@ssibrahimbas.com>
2024-01-16 21:07:26 +0300

    feature/cillop: add account delete cmd

--- app/command/account_delete.go
+++ app/command/account_delete.go
@@ -1,1 +1,35 @@
 package command
+
+import (
+	"context"
+
+	"github.com/cilloparch/cillop/cqrs"
+	"github.com/cilloparch/cillop/i18np"
+	"github.com/turistikrota/service.account/domains/account"
+)
+
+type AccountDeleteCmd struct {
+	UserUUID string `params:"-"`
+	UserName string `params:"userName" validate:"required,username"`
+}
+
+type AccountDeleteRes struct{}
+
+type AccountDeleteHandler cqrs.HandlerFunc[AccountDeleteCmd, *AccountDeleteRes]
+
+func NewAccountDeleteHandler(repo account.Repo, events account.Events) AccountDeleteHandler {
+	return func(ctx context.Context, cmd AccountDeleteCmd) (*AccountDeleteRes, *i18np.Error) {
+		err := repo.Delete(ctx, account.UserUnique{
+			UUID: cmd.UserUUID,
+			Name: cmd.UserName,
+		})
+		if err != nil {
+			return nil, err
+		}
+		events.Deleted(account.UserUnique{
+			UUID: cmd.UserUUID,
+			Name: cmd.UserName,
+		})
+		return &AccountDeleteRes{}, nil
+	}
+}
