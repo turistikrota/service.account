00d823792fa0e734dac5e3b0297b3e81a7c836e1
9ssi7 <info@ssibrahimbas.com>
2024-01-16 21:48:51 +0300

    feature/cillop: add cqrs app

--- app/app.go
+++ app/app.go
@@ -1,1 +1,27 @@
 package app
+
+import (
+	"github.com/turistikrota/service.account/app/command"
+	"github.com/turistikrota/service.account/app/query"
+)
+
+type Application struct {
+	Commands Commands
+	Queries  Queries
+}
+
+type Commands struct {
+	AccountDelete  command.AccountDeleteHandler
+	AccountCreate  command.AccountCreateHandler
+	AccountUpdate  command.AccountUpdateHandler
+	AccountEnable  command.AccountEnableHandler
+	AccountDisable command.AccountDisableHandler
+}
+
+type Queries struct {
+	AccountFilter      query.AccountFilterHandler
+	AccountGet         query.AccountGetHandler
+	AccountProfileView query.AccountProfileViewHandler
+	AccountListMy      query.AccountListMyHandler
+	AccountListByIds   query.AccountListByIdsHandler
+}
